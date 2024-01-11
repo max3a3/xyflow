0e9afe425636c489efc4186c139ddd0a7d0604c0
jin <jiinliin@gmail.com>
2024-01-11 10:18:25 +0700

    get simpletest tailwind running

---
+++ examples/simple-test/tailwind.config.cjs
@@ -0,0 +1,14 @@
+import { radixThemePreset } from 'radix-themes-tw';
+
+module.exports = {
+    // presets: [radixThemePreset],
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+
+    darkMode: 'class', // or 'media' or 'class'
+
+    theme: {
+        extend: {
+        }
+    },
+}
+                
